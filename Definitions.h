462f4776814c1634ca9971a29d0e2f4795501a36
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2021-03-04 17:49:50 +0530

    made definition types

--- Definitions.h
+++ Definitions.h
@@ -5,7 +5,57 @@
 #define NAME_LEN 100
 #define SYM_TABLE_SIZE 40
 
-
+#define astProgram 500
+#define astFunctions 501
+#define astFunction 502
+#define astFunctionName 503
+#define astParamList 504
+#define astStmtsList 505
+#define astBreak 506
+#define astContinue 507
+#define astAssignStmt 508
+#define astLoop 509
+#define astConditional 510
+#define astRemaiCond 511
+#define astElifStmts 512
+#define astElseStmt 513
+#define astConditions 514
+#define astBoolean 515
+#define astReturnStmt 516
+#define astArrayDecl 517
+#define astFuncCall 518
+#define astCustomFunc 519
+#define astFuncShow 520
+#define astFuncTake 521
+#define astArgs 522
+#define astArrayAssign 523
+#define astIdList 524
+#define astParam 525
+#define astAssignment 526
+#define astExpr 527
+#define astArr 528
+#define astData 529
+#define astInt 530
+#define astBool 531
+#define astStr 532
+#define astDouble 533
+#define astVoid 534
+#define astSub 535
+#define astMul 536
+#define astDiv 537
+#define astLte 538
+#define astGte 539
+#define astLt 540
+#define astGt 541
+#define astEq 542
+#define astNeq 543
+#define astAnd 544
+#define astOr 545
+#define astXor 546
+#define astIntConst 547
+#define astStrConst 548
+#define astBoolConst 549
+#define astFloatConst 550
 
 union Value{
   int ivalue;
